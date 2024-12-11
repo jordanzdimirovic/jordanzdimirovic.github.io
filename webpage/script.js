a93926398cfe37501d9f2aeaaa6142704925e679
Jordan Zdimirovic <jordan@Jordans-MacBook-Pro.local>
2024-12-11 13:46:53 +1100

    Blah

--- webpage/script.js
+++ webpage/script.js
@@ -1,5 +1,7 @@
-alert("Dismiss to run...");
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", "http://localhost:5000/test", false ); // false for synchronous request
-xmlHttp.send( null );
-return xmlHttp.responseText;
+function runme(){
+    alert("Dismiss to run...");
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "http://localhost:5000/test", false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
