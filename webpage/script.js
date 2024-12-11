6c200eac5de6e559af800cebbc7007aecf2a34ec
Jordan Zdimirovic <jordan@Jordans-MacBook-Pro.local>
2024-12-11 14:00:06 +1100

    hi

--- webpage/script.js
+++ webpage/script.js
@@ -1,7 +1,7 @@
 let runme = function (){
     alert("Dismiss to run...");
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "http://localhost:5000/test", false ); // false for synchronous request
+    xmlHttp.open("GET", "http://localhost:5001/test", false ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
